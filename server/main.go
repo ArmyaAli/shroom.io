147ac286bf49a99d4cba38589cda39e0b730ff2e
Ali Umar <ali.umar.work@gmail.com>
2024-01-13 11:09:40 -0500

    add client registration endpoint

--- server/main.go
+++ server/main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	// Pocketbase
 	"game_server/pkg/websocket"
+	"github.com/labstack/echo/v5"
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
@@ -17,18 +18,24 @@ func main() {
 
 	app := pocketbase.New()
 
+	registerChannel := make(chan string, 2048)
 	// serves static files from the provided public dir (if exists)
 	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
 
 		e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./pb_public"), false))
 		// Register our websocket endpoint
-		e.Router.GET("/websocket", websocket.Init_ws)
+		e.Router.GET("/register", func(c echo.Context) error { 
+      return websocket.RegisterClient(c, registerChannel) 
+    })
+		e.Router.GET("/websocket", func(c echo.Context) error { 
+      return websocket.InitWebsocket(c, registerChannel) 
+    })
 
 		return nil
 	})
-  
-  if err := app.Start(); err != nil {
-    fmt.Print("Hello")
-  }
+
+	if err := app.Start(); err != nil {
+		fmt.Print("Hello")
+	}
 
 }
